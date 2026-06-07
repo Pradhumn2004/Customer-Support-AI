f717d2de396c198b0c3a4f902c53b9e336e4a86b
Pradhumn2004 <pradhumnvyas07@gmail.com>
2026-06-07 16:06:45 +0530

    Deployment

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.app.api import chat, documents, tickets
@@ -10,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:3000").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
